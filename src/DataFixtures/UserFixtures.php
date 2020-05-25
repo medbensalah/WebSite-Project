910c961a12e9fd36d9bb2f3a1565e8d909f650f2
Mohamed <mohamedbensalah@insat.u-carthage.tn>
2020-05-25 22:51:05 +0100

    fakeUsers

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,10 +4,11 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(\Doctrine\Persistence\ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
