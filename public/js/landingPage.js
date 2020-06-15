aa7ebc9bc46bf765451aa9eb7c90753b0d4f10b1
root <mohamedbensalah@insat.u-carthage.tn>
2020-06-15 23:41:07 +0100

    userSideFinal

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -4,14 +4,11 @@ function myFunction() {
     console.log(3);
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
 var dropOpen = document.getElementsByClassName('show');
-console.log(dropOpen[0].classList)
-            if (!event.target.classList.contains('badge')) {
-                dropOpen[0].classList.remove('show');
-                console.log('here');
-            }else{
-                console.log('here2');
-    }
+var t = event.target;
+if(typeof t !== "undefined") {
+    if (!t.classList.contains('badge')) {
+        dropOpen[0].classList.remove('show');
+}
 }
